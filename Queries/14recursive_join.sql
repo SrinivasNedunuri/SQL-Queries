3de3c862f18f1cb893f13573c031345d5b63f434
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:28:46 -0400

    Update 14recursive_join.sql

--- Queries/14recursive_join.sql
+++ Queries/14recursive_join.sql
@@ -1,3 +1,26 @@
+/*Continued from problem 13, assumeing we treat the years from beginning of a
+title until promotion as the distance between nodes. e.g. An employee started as
+an Assistant Engineer from 1950-01-01 to 1955-12-31 then be promoted to Engineer on 1955-12-31. Then there is an edge between node "Assistant Engineer"
+to "Engineer" with distance 6.
+Calculate the average distance of all possible pair of titles and ordered by
+source node. To simplify the problem, there is no need to consider months and
+date when calculating the distance. Only year is required for calculating the
+distance. Besides, we can assume the distances of any given pair is less than
+100.
+Sort the result by src then dst. The expected result is shown as follow:
+src dst years
+Assistant Engineer Engineer 7.7926
+Assistant Engineer Manager 20.5266
+...
+Engineer Manager 12.7340
+...
+As the table shows, the average distance between node "Assistant Engineer" and
+node "Engineer" is 7.7926. We add it with the distance between "Engineer"
+to "Manager", which is 12.7340, to find out the distance between "Assistant
+Engineer" to "Manager" is 20.5266.*/
+
+
+
 WITH recursive TitleCTE AS (
 	select t1, t2, avg(years) + 1 years 
     from (select t1.title t1, t2.title t2, year(t2.from_date) - year(t1.from_date) years
