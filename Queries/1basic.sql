656fb6f9f2cb0025d19a6ed63b4ca850e52712d8
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:29:26 -0400

    Update 1basic.sql

--- Queries/1basic.sql
+++ Queries/1basic.sql
@@ -1,1 +1,8 @@
+/*Find all employees’ employee number, birth date, gender. Sort the result by
+employee number. The result of query is similar to following table:
+emp_no birth_date gender
+10001 1953-09-02 M
+10002 1964-06-02 F
+10003 1959-12-03 M*/
+
 select emp_no, birth_date, gender from employees
