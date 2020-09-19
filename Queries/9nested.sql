00a7e3771c30f63b20446174278e3040cb5194a6
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:36:28 -0400

    Update 9nested.sql

--- Queries/9nested.sql
+++ Queries/9nested.sql
@@ -1,3 +1,15 @@
+/*For each employee, find out how many times the title has been changed without
+chaning of the salary. e.g. An employee promoted from Engineer to Sr. Engineer
+with salaries remains 10k. Sort the result by employ number. The result of query
+is similar to following table:
+emp_no cnt
+10004 1
+10005 1
+10007 1
+10009 2
+...*/
+
+
 select emp_no, count(*) cnt 
 from titles 
 where from_date not in 
