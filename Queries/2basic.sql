e14d3e710dc53288cf71d7c38ac612ce82946150
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:30:10 -0400

    Update 2basic.sql

--- Queries/2basic.sql
+++ Queries/2basic.sql
@@ -1,1 +1,9 @@
+/*Find all female employees and sort the result by employee number. The result
+of query is similar to following table:
+emp_no birth_date first_name last_name gender hire_date
+10002 1964-06-02 Bezalel Simmel F 1985-11-21
+10006 1953-04-20 Anneke Preusig F 1989-06-02
+...*/
+
+
 select * from employees where gender = 'F'
