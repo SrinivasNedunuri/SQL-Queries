2058e59b1c58d9d5c413f37a4dd62a24ff18ef65
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:34:13 -0400

    Update 6join.sql

--- Queries/6join.sql
+++ Queries/6join.sql
@@ -1,3 +1,16 @@
+/*List pairs of employee (e1; e2) which satisfies ALL following conditions:
+1. Both e1 and e2’s current department number is d001.
+2. The year of birthdate for e1 and e2 is 1955.
+3. The e1’s employee number is less than e2.
+Sort the result by e1 then e2. The result of query is similar to following table:
+e1 e2
+10239 10367
+10239 11251
+...
+10367 11251
+10367 11554
+...*/
+
 select t1.emp_no e1, t2.emp_no e2 
 from dept_emp t1, dept_emp t2, employees t3, employees t4
 where t1.dept_no = 'd001' and 
