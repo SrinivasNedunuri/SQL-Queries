a15aefac25277e5c684ce9c0da5c7d7ce86519dd
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 19:35:31 -0400

    Update 8nested.sql

--- Queries/8nested.sql
+++ Queries/8nested.sql
@@ -1,3 +1,10 @@
+/*Find out departments which has changed its manager more than once then list
+out the name of the departments and the number of changes. Sort the result
+by department name. The result of query is similar to following table:
+dept_name cnt
+Customer Service 3
+...*/
+
 select q2.dept_name, q2.cnt from
 (select d.dept_name, count(*) cnt from
 (select * from dept_manager where to_date <> '9999-01-01') q1, departments d 
